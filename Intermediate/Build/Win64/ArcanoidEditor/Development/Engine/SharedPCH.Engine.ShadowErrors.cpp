bf4660dc9a260f0fb7c838158632346835a99a6c
AbirCv-boeck <abircvonboeck@gmail.com>
2022-10-12 01:13:24 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/ArcanoidEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/DELL/Documents/Unreal Projects/Arcanoid/Intermediate/Build/Win64/ArcanoidEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/Arcanoid/PaddlePlayerController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PaddlePlayerController.h"
+
+void APaddlePlayerController::BeginPlay()
+{
+	TArray<AActor*> CameraActors;
+
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACameraActor::StaticClass(), CameraActors);
+
+	FViewTargetTransitionParams Params;
+	SetViewTarget(CameraActors[0], Params);
+}
